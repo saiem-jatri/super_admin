a4eed7f2404cfb8ebe27ef149e2849dc6aedb8df
khalid-jatri <khalidhossain.jatri@gmail.com>
2022-10-16 10:33:43 +0600

    busCompanyTabsRoute issue fixing complete

--- src/views/bus_company_create_counter.vue
+++ src/views/bus_company_create_counter.vue
@@ -1,49 +1,39 @@
 <template>
       <!-- Top nav bar start-->
       <div class="bg-white h-[72px] sticky top-0 border-b border-[#DBDBDB] z-10 px-[32px]">
-        <div class="flex justify-between">
-          <div class="flex justify-center items-center gap-x-[32px]">
+      <div class="flex justify-between">
+        <div class="flex justify-center items-center gap-x-[32px]">
 
-            <button class="flex justify-center items-center gap-x-[10px]">
-              <img src="../assets/svg/Back.svg" class="w-[12px] h-[12px]" alt="">
-              <p class="text-primaryText leading-[12px] text-[10px] xl:leading-[16px] xl:text-[12px] font-[500]">Back</p>
-            </button>
-            <div class="flex flex-col gap-y-[4px]">
-              <p class="lg:leading-[16px] xl:leading-[28px] lg:text-[14px] xl:text-[20px] font-[600] text-primaryText">Company</p>
-              <div class="flex justify-center items-center gap-x-[6px]">
-                <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Desh Travels Limited</p>
-                <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
-                <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Manage</p>
-                <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
-                <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Desh Travels Limited</p>
-                <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
-                <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Route</p>
-                <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
-                <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Add route</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex gap-x-[24px] p-4 w-1/2 justify-end items-center pr-2 ">
-            <!--            <input type="text" class="w-[270px] h-[40px] border border-[#DBDBDB] placeholder:text-[#4D4D4F] placeholder:text-[14px] leading-[20px] rounded-[4px] pl-8 focus:outline-none" placeholder="PNR or mobile number">-->
-            <!--            <img src="../assets/svg/search.svg" class="h-5 w-5 absolute left-[242px]" alt="">-->
-            <div class="w-[200px] h-[30px] xl:w-[270px] xl:h-[40px] border border-[#DBDBDB] flex justify-center items-center leading-[20px] rounded-[4px] pl-[9px] overflow-hidden" >
-              <img src="../assets/svg/search.svg" class="h-2 w-2 xl:h-5 xl:w-5" alt="">
-              <input type="text" class="outline-none pl-32 xl:pl-2 placeholder:text-[#4D4D4F] placeholder:text-[8px] placeholder:leading-[16px] xl:placeholder:text-[14px] xl:placeholder:leading-[20px]" placeholder="PNR or mobile number">
-            </div>
-            <div class="flex justify-center items-center gap-x-1">
-              <img src="../assets/person/person1.png" alt="" class="h-[30px]  w-[30px] xl:h-[40px] xl:w-[40px] rounded-full">
-              <p class="text-[#151414] leading-[16px] text-[10px] xl:leading-[24px] xl:text-[16px] font-[500]">Mostafa Mamun Emon</p>
-              <img src="../assets/person/downArrow.svg" alt="" class="w-[12px] h-[6px]">
+          <button class="flex justify-center items-center gap-x-[10px]">
+            <img src="../assets/svg/Back.svg" class="w-[12px] h-[12px]" alt="">
+            <p class="text-primaryText leading-[12px] text-[10px] xl:leading-[16px] xl:text-[12px] font-[500]">Back</p>
+          </button>
+          <div class="flex flex-col gap-y-[4px]">
+            <p class="lg:leading-[16px] xl:leading-[28px] lg:text-[14px] xl:text-[20px] font-[600] text-primaryText">Counter</p>
+            <div class="flex justify-center items-center gap-x-[6px]">
+              <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Company</p>
+              <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
+              <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Desh Travels Limited</p>
+              <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
+              <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Manage</p>
+              <img src="../assets/svg/bgBack.svg" alt="" class="w-1 h-2">
+              <p class="text-secondaryText leading-[12px] text-[8px] xl:leading-[16px] xl:text-[12px] font-[400]">Counter</p>
             </div>
           </div>
         </div>
+        <div class="flex gap-x-2 p-4 justify-center items-center">
+            <img src="../assets/person/person1.png" alt="" class="h-[30px] xl:h-[40px] w-[30px] xl:w-[40px] rounded-full">
+            <p class="text-[#151414] leading-[20px] text-[12px] xl:leading-[24px] xl:text-[16px] font-[500]">Mostafa Mamun Emon</p>
+            <img src="../assets/person/downArrow.svg" alt="" class="w-[12px] h-[6px]">
+          </div>
       </div>
+    </div>
       <!-- top nav bar end -->
       <!-- body start-->
       <div class="px-[30px] py-[24px]">
 
         <!--        card-->
-        <div class="mt-[16px] bg-white rounded-[10px]">
+        <div class="bg-white rounded-lg">
           <div class="p-4">
           <div class="flex justify-center items-center gap-x-[10px] xl:gap-x-[16px]">
             <div class="w-1/4">
