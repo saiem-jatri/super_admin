88019e427ec3f58a2a994f0838efb75dc7d00b76
khalid-jatri <khalidhossain.jatri@gmail.com>
2022-10-19 17:05:07 +0600

    tripExpenseConfiguration pages issues fixed

--- src/views/tripExpenseConfiguration.vue
+++ src/views/tripExpenseConfiguration.vue
@@ -13,15 +13,16 @@
           </div>
         </div>
         <!-- top nav bar end -->
+
         <!-- body start-->
         <div class="px-[30px] py-[24px]">
-  <!--        card-->
+          <!--card-->
           <div class="bg-white rounded">
             <div class="p-4">
                   <div class="flex justify-between items-center gap-x-4 w-full">
                   <div class="w-1/4 xl:w-1/5">
                     <p class="text-primaryText leading-[20px] text-[14px] xl:leading-[24px] xl:text-[16px] font-[600]">Bus operator</p>
-                    <Dropdown class="w-full py-1 mt-[10px] placeholder:text-primaryText placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Dash Travels" />
+                    <Dropdown class="w-full py-1 mt-[10px] placeholder:text-primaryText placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select operator" />
                   </div>
                   <div class="flex items-end self-end gap-[10px] hidden">
                     <img src="../assets/svg/refresh.svg" alt="Refresh Img">
@@ -33,16 +34,16 @@
 
                 <div class="p-4 bg-corporateBg rounded my-4 flex justify-between items-center">
                   <div class="flex items-center gap-x-4 w-1/2">
-                    <div>
+                    <div class="mr-1">
                       <img src="../assets/svg/hamburger.svg" alt="">
                     </div>
                     <div class="w-1/2">
                     <p class="text-primaryText leading-[16px] text-[12px] xl:leading-[20px] xl:text-[14px] font-[600]">Field name</p>
-                    <input type="text" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="Oil">
+                    <input type="text" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="Enter field name">
                     </div>
                     <div class="w-1/2">
                     <p class="text-primaryText leading-[16px] text-[12px] xl:leading-[20px] xl:text-[14px] font-[600]"> Maximum value (TK)</p>
-                    <input type="number" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="5000">
+                    <input type="number" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="Enter value">
                     </div>
                   </div>
                   <div class="hidden">
@@ -51,16 +52,16 @@
                 </div>
                 </div>
                 
-                <router-link to="/tripExpenseConfigurationFilled" class="w-full border border-dashed border-[#B6B6B6] flex justify-center items-center bg-corporateBg rounded-[4px]"> 
+                <router-link to="/tripExpenseConfigurationFilled" class="w-full border border-dashed border-corporateSecondaryBlue flex justify-center items-center bg-[#EFF7FD] rounded-[4px]"> 
                   <div class="flex justify-center items-center gap-x-[11px] py-[12px]"><img alt="" class="w-[10px] h-[10px]" src="/src/assets/svg/createPlus.svg">
-                    <p class="text-primaryText font-[600] leading-[20px] text-[14px]">Add another agent</p>
+                    <p class="text-primaryText font-[600] leading-[20px] text-[14px]">Add another filed</p>
                   </div>
                 </router-link>
                 </div>
 
                 <hr class="border-t border-corporateBorder">
                 <div class="px-[16px] py-[20px] w-full flex justify-center items-center">
-                  <router-link to="/tripExpenseConfigurationFilled" class="lessOpacityCorporateButton">Create routes</router-link>
+                  <router-link to="/tripExpenseConfigurationFilled" class="lessOpacityCorporateButton">Create expense</router-link>
                 </div>
           </div>
         </div>
--- src/views/tripExpenseConfigurationFilled.vue
+++ src/views/tripExpenseConfigurationFilled.vue
@@ -13,15 +13,16 @@
         </div>
       </div>
       <!-- top nav bar end -->
+      
       <!-- body start-->
       <div class="px-[30px] py-[24px]">
-<!--        card-->
+      <!--card-->
         <div class="bg-white rounded">
           <div class="p-4">
                   <div class="flex justify-between items-center gap-x-4 w-full">
                   <div class="w-1/4 xl:w-1/5">
                     <p class="text-primaryText leading-[20px] text-[14px] xl:leading-[24px] xl:text-[16px] font-[600]">Bus operator</p>
-                    <Dropdown class="w-full py-1 mt-[10px] placeholder:text-primaryText placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Dash Travels" />
+                    <Dropdown class="w-full py-1 mt-[10px] placeholder:text-primaryText placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select bus operator" />
                   </div>
                   <div class="flex items-end self-end gap-[10px]">
                     <img src="../assets/svg/refresh.svg" alt="Refresh Img">
@@ -30,18 +31,18 @@
                 </div>
 
                 <div>
-                  <div class="p-4 bg-corporateBg rounded my-4 flex justify-between items-center">
+                  <div class="p-4 bg-corporateBg rounded-[6px] my-4 flex justify-between items-center">
                   <div class="flex items-center gap-x-4 w-1/2">
-                    <div>
+                    <div class="mr-1">
                       <img src="../assets/svg/hamburger.svg" alt="">
                     </div>
                     <div class="w-1/2">
                     <p class="text-primaryText leading-[16px] text-[12px] xl:leading-[20px] xl:text-[14px] font-[600]">Field name</p>
-                    <input type="text" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="Oil">
+                    <input type="text" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="Enter field name">
                     </div>
                     <div class="w-1/2">
                     <p class="text-primaryText leading-[16px] text-[12px] xl:leading-[20px] xl:text-[14px] font-[600]"> Maximum value (TK)</p>
-                    <input type="number" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="5000">
+                    <input type="number" class="focus:outline-none mt-[10px] w-full border border-[#DBDBDB] rounded-[4px] pl-2 py-2 placeholder:text-[#676769] placeholder:leading-[16px] placeholder:text-[12px] xl:placeholder:leading-[20px] xl:placeholder:text-[14px]" placeholder="Enter value">
                     </div>
                   </div>
                   <div class="hidden">
@@ -49,9 +50,9 @@
                   </div>
                 </div>
 
-                <div class="p-4 bg-corporateBg rounded my-4 flex justify-between items-center">
+                <div class="p-4 bg-corporateBg rounded-[6px] my-4 flex justify-between items-center">
                   <div class="flex items-center gap-x-4 w-1/2">
-                    <div>
+                    <div class="mr-1">
                       <img src="../assets/svg/hamburger.svg" alt="">
                     </div>
                     <div class="w-1/2">
@@ -69,9 +70,9 @@
                 </div>
                 </div>
                 
-                <button class="w-full border border-dashed border-[#B6B6B6] flex justify-center items-center bg-corporateBg rounded-[4px]"> 
+                <button class="w-full border border-dashed border-corporateSecondaryBlue flex justify-center items-center bg-[#EFF7FD] rounded-[4px]"> 
                   <div class="flex justify-center items-center gap-x-[11px] py-[12px]"><img alt="" class="w-[10px] h-[10px]" src="/src/assets/svg/createPlus.svg">
-                    <p class="text-primaryText font-[600] leading-[20px] text-[14px]">Add another agent</p>
+                    <p class="text-primaryText font-[600] leading-[20px] text-[14px]">Add another filed</p>
                   </div>
                 </button>
                 </div>
@@ -84,7 +85,7 @@
               </div>
         </div>
 
-        <div class="bg-white rounded mt-4">
+        <div class="bg-white rounded-[10px] mt-4">
           <div class="p-6">
             <div class="gap-x-4 w-full">
               <p class="text-primaryText mb-6 leading-[20px] text-[14px] xl:leading-[24px] xl:text-[16px] font-[600]">Preview</p>
